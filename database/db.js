5eb488e0db95d7767b0cfcbf2fc8bd9b89068796
Yeom Ye Chan <yyc2911@salin.co.kr>
2022-06-01 20:58:28 +0900

    유신 노드 작업

---
+++ database/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+function connectDB() {
+    return mongoose.connect("mongodb+srv://test:sparta@cluster0.rx7dw.mongodb.net/?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
+
+module.exports = connectDB;
+module.exports = connectDB;
